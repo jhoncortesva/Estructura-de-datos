db8fa517c949c4470d5e6aaf563630c99ffc9b5f
JuanChica7 <112190801+JuanChica7@users.noreply.github.com>
2022-10-22 19:08:46 -0500

    ahi hice lo que pue jajaj

--- javascrikt.js
+++ javascrikt.js
@@ -7,4 +7,46 @@ console.log(frutas)
 // pilas
 
 
+class node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    add(value) {
+        if (value < this.value) {
+            this.addToTheLeft(value);
+        } else {
+            this.addToTheRight(value);
+        }
+    }
+
+    addToTheLeft(value) {
+        if (this.left) {
+            this.left.add(value);
+        } else {
+            this.left = new node(value);
+        }
+    }
+
+    addToTheRight(value) {
+        if (this.right) {
+            this.right.add(value);
+        } else {
+            this.right = new node(value);
+        }
+    }
+}
+
+const head = new node(10);
+head.add(15);
+head.add(9);
+head.add(8);
+head.add(9);
+head.add(10);
+head.add(16);
+head.add(7);
+ console.log(head);
+
 
