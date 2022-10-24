81240d44a96e650d9a0bbf0a419423423f9a53ad
jhoncortesva <112190515+jhoncortesva@users.noreply.github.com>
2022-10-24 17:37:57 -0500

    se arregló el error

--- ejemplos siplasplas.cpp
+++ ejemplos siplasplas.cpp
@@ -7,9 +7,9 @@ int main () {
   int numerote = 5;               // Integer (whole number)
   float numeroteflotante = 5.99;     // Floating point number
   double myDoubleNum = 9.98;   // Floating point number
-  char letrota = 'D';         // Character
-  bool elbuliano = true;       // Boolean
-  string lacadeniñadeteksto = "Bololoña";   // String
+  char letrota = 'N';         // Character
+  bool elbuliano = false;       // Boolean
+  string cadena = "AEIOU";   // String
    
   // Print variable values
   cout << "int: " << numerote << "\n";
@@ -17,7 +17,7 @@ int main () {
   cout << "double: " << myDoubleNum << "\n";
   cout << "char: " << letrota<< "\n";
   cout << "bool: " << elbuliano << "\n";
-  cout << "string: " << lacadeniñadeteksto << "\n";
+  cout << "string: " << cadena << "\n";
  
   return 0;
 }
