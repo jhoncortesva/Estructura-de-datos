5c86cdecf758a983c05363119288759c86b72a82
jhoncortesva <112190515+jhoncortesva@users.noreply.github.com>
2022-10-28 11:22:03 -0500

    arrays

--- ejemplos siplasplas.cpp
+++ ejemplos siplasplas.cpp
@@ -10,14 +10,17 @@ int main () {
   char letrota = 'N';         // Caracter
   bool elbuliano = false;       // Booleano
   string cadena = "AEIOU";   // Cadena
+  string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
    
   // Mostrar variables
+  cout << cars[0] << "\n";
+  cout << cars[1] << "\n";
   cout << "Entero: " << numerote << "\n";
   cout << "flotante: " << numeroteflotante << "\n";
   cout << "double: " << myDoubleNum << "\n";
   cout << "caracter: " << letrota<< "\n";
   cout << "booleano: " << elbuliano << "\n";
   cout << "cadena: " << cadena << "\n";
- 
+  
   return 0;
 }
